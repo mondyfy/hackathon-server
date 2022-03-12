6c5f8777c41ac3326d31d0187f4ff86ee390eba2
mondyfy <khaptadisandip@gmail.com>
2022-03-12 17:31:42 +0545

    add auth and logger middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configService } from './config/config.service';
+import { AuthMiddleware } from './modules/middlewares/auth.middleware';
+import { LoggerMiddleware } from './modules/middlewares/logger.middleware';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
@@ -15,4 +17,15 @@ import { UserModule } from './modules/user/user.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: 'api/v1/auth/(.*)', method: RequestMethod.POST })
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
